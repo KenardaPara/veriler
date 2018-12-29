9374eb13d7a2c17ab815e2f996c8981f7d4dd5f7
VaryemezAmca <coder@kenardapara.com>
2018-12-29 20:15:17 +0100

    Cont

--- push.sh
+++ push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_files() {
-  git checkout travis-test  
+  git checkout -b travis-test2
   git add testdata
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/KenardaPara/veriler.git > /dev/null 2>&1
-  git push --set-upstream origin travis-test
+  git push --set-upstream origin travis-test2
 }
 
 setup_git
